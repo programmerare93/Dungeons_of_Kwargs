d4f5e2dc856ef261a93ba534c55095014c6033d3
simon <simon.rehn1@gmail.com>
2022-11-09 20:41:19 +0100

    Uppdaterade huvud loopen

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 from window.window import *
+from sys import exit
 
 window = Window("test", 800, 600)
-tile_set = TileSet("../assets/ibm_pc_font.png")
 
 icon = pygame.image.load("../assets/fort_icon.png")
 window.set_icon(icon)
 
 # Main-loopen
-while window.is_running:
+while True:
     for event in window.get_event():
         if window.should_quit(event):
-            window.is_running = False
+            exit()
+    window.update()
