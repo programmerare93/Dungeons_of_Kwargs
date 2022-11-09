6e13aa8c5d49ca861bca72897b0146bfc262b894
simon <simon.rehn1@gmail.com>
2022-11-09 20:16:34 +0100

    Initializerar pygame i __init__.py istället för window

--- src/window/window.py
+++ src/window/window.py
@@ -12,8 +12,6 @@ class Window:
 
     def __init__(self, title: str, width: int, height: int):
         """Constructor som initializerar pygame och skapar fönstret"""
-        pygame.init()
-
         self.screen = pygame.display.set_mode((width, height))
         pygame.display.set_caption(title)
 
@@ -34,3 +32,38 @@ class Window:
             return True
         else:
             return False
+
+
+@dataclass
+class TileSet:
+    file: str
+    size: tuple[int, int]
+    margin: int
+    spacing: int
+    image: pygame.image
+    rect: pygame.rect
+    tiles: list
+
+    def __init__(self, file: str, size=(8, 8), margin=1, spacing=1):
+        self.file = file
+        self.size = size
+        self.margin = margin
+        self.spacing = spacing
+        self.image = pygame.image.load(file)
+        self.rect = self.image.get_rect()
+        self.tiles = []
+
+        self.load()
+
+    def load(self):
+        self.tiles = []
+        x0 = y0 = self.margin
+        w, h = self.rect.size
+        dx = self.size[0] + self.spacing
+        dy = self.size[1] + self.spacing
+
+        for x in range(x0, w, dx):
+            for y in range(y0, h, dy):
+                tile = pygame.Surface(self.size)
+                tile.blit(self.image, (0, 0), (x, y, *self.size))
+                self.tiles.append(tile)
