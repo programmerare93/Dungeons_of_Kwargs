2ca961ff6fbc47bfb4a9107877eca9989143273b
simon <simon.rehn1@gmail.com>
2022-11-20 20:59:09 +0100

    Lade till fällor

--- src/stage/tile_types.py
+++ src/stage/tile_types.py
@@ -1,3 +1,4 @@
+import random
 from typing import Tuple
 
 from tcod import Console
@@ -57,6 +58,8 @@ class Wall(Tile):
         self.has_monster = False
 
 
+# Vi har inget specifikt objekt för att kopiera för Trap
+# då det resulterar i att all fällor har samma svårighet
 class Trap(Tile):
     def __init__(self, color):
         self.walkable = True
@@ -67,6 +70,8 @@ class Trap(Tile):
         self.char = "."
         self.type = 2
         self.has_monster = False
+        self.difficulty = random.randint(6, 10)
+        self.hasBeenActivated = False
 
 
 class StairCase(Tile):
@@ -86,12 +91,10 @@ types_of_tiles = {"floor": 0, "wall": 1, "trap": 2, "stair": 3}
 seen_color = (55, 55, 55)
 floor_color = (155, 200, 255)
 wall_color = (255, 255, 255)
-trap_color = (170, 210, 255)
+trap_color = (0, 210, 0)
 
 floor = Floor(floor_color)
 
 wall = Wall(wall_color)
-
-trap = Trap(trap_color)
 
 stair_case = StairCase(wall_color)
